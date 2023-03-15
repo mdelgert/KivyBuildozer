4f4f59b586287f002c94a33d99a002d58fccb342
Matthew David Elgert <mdelgert@yahoo.com>
2023-03-15 13:44:11 -0400

    Added additional docker examples

--- build.ps1
+++ build.ps1
@@ -4,4 +4,8 @@ docker stop test
 
 docker rm test
 
-docker run -d -p 22:22 --name test kivybuildozer:1.0
+#docker run -d -p 22:22 --name test kivybuildozer:1.0
+
+#https://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
+
+docker run -d --name test kivybuildozer:1.0
